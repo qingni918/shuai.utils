bd9e992405b05fd5727200a24eba5df174471337
qingni918 <91673424+qingni918@users.noreply.github.com>
2024-09-05 10:21:08 +0800

    002

--- utils.go
+++ utils.go
@@ -1,7 +1,42 @@
 package shuai_utils
 
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+)
+
 func Panic(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func EncodeGzip(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	writer := gzip.NewWriter(&buf)
+	_, err := writer.Write(data)
+	if err != nil {
+		return nil, err
+	}
+	// 确保所有数据都被写入
+	if err = writer.Close(); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func DecodeGzip(data []byte) ([]byte, error) {
+	reader, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, reader)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
--- utils_test.go
+++ utils_test.go
@@ -11,3 +11,17 @@ func TestPanic(t *testing.T) {
 	err = fmt.Errorf("err")
 	Panic(err)
 }
+
+func TestEncodeGzip(t *testing.T) {
+	str := []byte(`
+{"level":"debug","serverTime":"2024-09-05T10:18:48.871+0800","serviceID":"matchshared-10","file":"server/httpserver.go:48","message":"request processed","_pos":"[httpserver.go:1176]","_REQ":"action=share_team_del&caller=match-11&match_phase=rival&match_type=MTYPE_PVE_TRAINING&team_id=118","_REP":"{\"Result\":0,\"Desc\":\"ok\"}"}
+`)
+	fmt.Println("source len:", len(str))
+	encodeStr, err := EncodeGzip(str)
+	Panic(err)
+	fmt.Println("encode:", len(encodeStr))
+
+	decodeStr, err := DecodeGzip(encodeStr)
+	Panic(err)
+	fmt.Println("decode:", string(decodeStr))
+}
